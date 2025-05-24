2e7316f1344b585b5237d431adb6d555011c6e11
Faros <114036707+a-faros@users.noreply.github.com>
2025-05-24 19:43:26 +0800

    Create StaffAttribute.php

---
+++ App/Models/StaffAttribute.php
@@ -0,0 +1,14 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class StaffAttribute extends Model
+{
+    protected $fillable = ['key', 'value'];
+
+    public function attributable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
